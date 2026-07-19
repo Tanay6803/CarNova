7c1b74af799399182bc8451c3017f4edfdf4af3b
Tanay Pandey <tanayrocket6803@gmail.com>
2026-07-19 17:45:22 +0530

    Resolving the issue with the images

--- backend/urls.py
+++ backend/urls.py
@@ -27,8 +27,8 @@ urlpatterns = [
 
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+# Always serve uploaded media
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
