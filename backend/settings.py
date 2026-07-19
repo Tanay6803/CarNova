3d8503da56a7ec879f483c9aaa11cd0e8afefd14
Tanay Pandey <tanayrocket6803@gmail.com>
2026-07-19 15:36:45 +0530

    configure project for render deployment

--- backend/settings.py
+++ backend/settings.py
@@ -15,10 +15,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-_ar#^78l^t_ndj8#!yo2v!s10l2*mubbj)3i@zy0wpl8ycrd&9"
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com",
+]
 
 # ==========================================================
 # INSTALLED APPLICATIONS
